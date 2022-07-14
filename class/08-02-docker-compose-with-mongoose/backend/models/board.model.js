5b64f16dfe8ab8aeb05bb6cc1b97c919c3a621ef
nwd0907 <nwd0907@newbizstart.com>
2022-07-14 19:17:50 +0900

    Update crawler

--- class/08-02-docker-compose-with-mongoose/backend/models/board.model.js
+++ class/08-02-docker-compose-with-mongoose/backend/models/board.model.js
@@ -2,6 +2,8 @@ import mongoose from 'mongoose'
 
 const BoardSchema = new mongoose.Schema({
     writer: String,
+    title: String,
+    contents: String
 })
 
 export const Board = mongoose.model("Board", BoardSchema)
