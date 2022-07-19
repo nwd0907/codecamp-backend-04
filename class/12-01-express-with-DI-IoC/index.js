aa19a443c54eb14763c3c6f1903659212aedb446
nwd0907 <nwd0907@newbizstart.com>
2022-07-19 21:14:51 +0900

    Fix

--- class/12-01-express-with-DI-IoC/index.js
+++ class/12-01-express-with-DI-IoC/index.js
@@ -9,9 +9,9 @@ import { PointService } from "./mvc/controllers/services/point.service.js";
 const app = express();
 
 // 서비스 의존성들
-const cashService = new CashService(); // new 한번으로 모든 곳에서 재사용 가능(싱글톤패턴)
-const productService = new ProductService(); // new 한번으로 모든 곳에서 재사용 가능(싱글톤패턴)
-const pointService = new PointService(); // 쿠폰 구매 방식이 포인트결제로 변경됨
+const pointService = new PointService(); // 1. 쿠폰 구매 방식이 포인트결제로 변경됨(의존성주입)
+const cashService = new CashService(); // 2. new 한번으로 모든 곳에서 재사용 가능(싱글톤패턴)
+const productService = new ProductService();
 
 // 상품 API
 const productController = new ProductController(cashService, productService);
--- class/12-05-typescript-private-readonly/index.ts
+++ class/12-05-typescript-private-readonly/index.ts
@@ -1,6 +1,6 @@
 // public, private, protected, readonly
 
-// // 1. public
+// 1. public
 // class Aaa1 {
 //   constructor(public mypower) {
 //     // this.mypower = mypower; // public, protected, private, readonly 등 1개만 포함되면 자동으로 셋팅됨!
@@ -85,8 +85,36 @@
 //
 //
 // 4. readonly
+// class Aaa1 {
+//   constructor(readonly mypower) {
+//     // this.mypower = mypower; // public, protected, private, readonly 등 1개만 포함되면 자동으로 셋팅됨!
+//   }
+//   ggg() {
+//     console.log(this.mypower); // 안에서 접근 가능
+//     this.mypower = 10; // 안에서 수정 불가
+//   }
+// }
+// class Aaa2 extends Aaa1 {
+//   ggg() {
+//     console.log(this.mypower); // 자식이 접근 가능
+//     this.mypower = 10; // 자식이 수정 불가
+//   }
+// }
+// const aaaa = new Aaa2(50);
+// console.log(aaaa.mypower); // 밖에서 접근 가능
+// aaaa.mypower = 10; // 밖에서 수정 불가
+
+//
+//
+//
+//
+//
+//
+//
+//
+// 5. private readonly
 class Aaa1 {
-  constructor(readonly mypower) {
+  constructor(private readonly mypower) {
     // this.mypower = mypower; // public, protected, private, readonly 등 1개만 포함되면 자동으로 셋팅됨!
   }
   ggg() {
@@ -96,10 +124,10 @@ class Aaa1 {
 }
 class Aaa2 extends Aaa1 {
   ggg() {
-    console.log(this.mypower); // 자식이 접근 가능
+    console.log(this.mypower); // 자식이 접근 불가
     this.mypower = 10; // 자식이 수정 불가
   }
 }
 const aaaa = new Aaa2(50);
-console.log(aaaa.mypower); // 밖에서 접근 가능
+console.log(aaaa.mypower); // 밖에서 접근 불가
 aaaa.mypower = 10; // 밖에서 수정 불가
