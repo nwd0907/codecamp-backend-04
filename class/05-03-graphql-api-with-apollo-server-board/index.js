2d81984185cd9670af6895957e84717da0afe3b6
nwd0907 <nwd0907@newbizstart.com>
2022-07-20 18:18:48 +0900

    Update nestjs-with-graphql

--- class/05-03-graphql-api-with-apollo-server-board/index.js
+++ class/05-03-graphql-api-with-apollo-server-board/index.js
@@ -1,6 +1,6 @@
 // const { ApolloServer, gql } = require('apollo-server');
-import { ApolloServer, gql } from 'apollo-server'
-import {checkValidationPhone, getToken, sendTokenToSMS} from './phone.js'
+import { ApolloServer, gql } from "apollo-server";
+import { checkValidationPhone, getToken, sendTokenToSMS } from "./phone.js";
 
 // The GraphQL schema
 const typeDefs = gql`
@@ -35,48 +35,63 @@ const resolvers = {
     fetchBoards: (parent, args, context, info) => {
       // 1. 데이터를 조회하는 로직 => DB에 접속해서 데이터 꺼내오기
       const result = [
-        { number: 1, writer: "철수", title: "제목입니다~~", contents: "내용이에요@@@" },
-        { number: 2, writer: "영희", title: "영희 제목입니다~~", contents: "영희 내용이에요@@@" },
-        { number: 3, writer: "훈이", title: "훈이 제목입니다~~", contents: "훈이 내용이에요@@@" }
-      ]
+        {
+          number: 1,
+          writer: "철수",
+          title: "제목입니다~~",
+          contents: "내용이에요@@@",
+        },
+        {
+          number: 2,
+          writer: "영희",
+          title: "영희 제목입니다~~",
+          contents: "영희 내용이에요@@@",
+        },
+        {
+          number: 3,
+          writer: "훈이",
+          title: "훈이 제목입니다~~",
+          contents: "훈이 내용이에요@@@",
+        },
+      ];
 
       // 2. 꺼내온 결과 응답 주기
-      return result
+      return result;
     },
   },
 
   Mutation: {
     createBoard: (_, args) => {
-      console.log(args.createBoardInput.writer)
-      console.log(args.createBoardInput.title)
-      console.log(args.createBoardInput.contents)
+      console.log(args.createBoardInput.writer);
+      console.log(args.createBoardInput.title);
+      console.log(args.createBoardInput.contents);
       // 1. 데이터를 등록하는 로직 => DB에 접속해서 데이터 저장하기
 
       // 2. 저장 결과 응답 주기
-      return "게시물 등록에 성공하였습니다!!"
+      return "게시물 등록에 성공하였습니다!!";
     },
 
     createTokenOfPhone: (_, args) => {
-      const myphone = args.myphone
+      const myphone = args.myphone;
 
       // 1. 휴대폰번호 자릿수 맞는지 확인하기
-      const isValid = checkValidationPhone(myphone)
-      if(isValid === false) return
+      const isValid = checkValidationPhone(myphone);
+      if (isValid === false) return;
 
       // 2. 핸드폰 토큰 6자리 만들기
-      const mytoken = getToken()
+      const mytoken = getToken();
 
       // 3. 핸드폰번호에 토큰 전송하기
-      sendTokenToSMS(myphone, mytoken)
-      return "인증완료!!!"
-    }
-  }
+      sendTokenToSMS(myphone, mytoken);
+      return "인증완료!!!";
+    },
+  },
 };
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  cors: true
+  cors: true,
 });
 
 server.listen(3000).then(() => {
